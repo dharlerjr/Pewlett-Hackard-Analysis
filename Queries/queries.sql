11a843eb41c26205364f9c132b4b0c8d83076576
dharlerjr <harler.david@gmail.com>
2022-06-05 10:32:12 -0700

    query data: retirement info.

---
+++ Queries/queries.sql
@@ -0,0 +1,9 @@
+-- Check Imports
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+
+-- Drop Statement for Reference: DROP TABLE employees CASCADE;
